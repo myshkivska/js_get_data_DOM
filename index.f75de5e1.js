e19b5d8383217a3377d3e672fdd9bac736d2e8aa
Yana Myshkivska <ymyshkivska@gmail.com>
2024-09-06 22:36:39 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const populationElements = document.querySelectorAll(".population");
+const populationNumbers = [];
+populationElements.forEach((element)=>{
+    const populationNumber = parseInt(element.textContent.replace(/,/g, ""), 10);
+    if (!isNaN(populationNumber)) populationNumbers.push(populationNumber);
+});
+if (populationNumbers.length > 0) {
+    let total = populationNumbers.reduce((acc, num)=>acc + num, 0);
+    let average = total / populationNumbers.length;
+    total = total.toLocaleString("en-US");
+    average = average.toLocaleString("en-US");
+    document.querySelector(".average-population").textContent = average.toLocaleString("en-US");
+    document.querySelector(".total-population").textContent = total.toLocaleString("en-US");
+} else {
+    document.querySelector(".average-population").textContent = "No data";
+    document.querySelector(".total-population").textContent = "No data";
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
